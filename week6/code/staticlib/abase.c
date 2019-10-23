acb267b26c513166b1fc29baa119081fe3728806
DZX <873109435@qq.com>
2019-10-22 22:04:31 -0700

    add week6

---
+++ week6/code/staticlib/abase.c
@@ -0,0 +1,17 @@
+void showarray(int *a,int n)
+{ 
+  int i;
+  for(i=0;i<n;i++)
+  {  
+    printf("%4d : %4d\n",i,a[i]);
+  }
+}
+void initarray(int *a,int n)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    a[i]=rand()%10;
+  }
+
+}
